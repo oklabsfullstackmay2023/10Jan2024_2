40dd6ef6dfe6ef63baf53acf4e88fe5e3af1b962
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2024-01-10 16:19:52 +0530

    'module use on classobject'

---
+++ a.py
@@ -0,0 +1,26 @@
+import b
+#2. create(create ceo) class external object is many time process
+ceo1=b.MyClass()
+ceo2=b.MyClass()
+ceo3=b.MyClass()
+ceo4=b.MyClass()
+
+print(ceo1.name)
+print(ceo1.surname)
+print(ceo1.age)
+print(ceo1.address)
+
+print(ceo2.name)
+print(ceo2.surname)
+print(ceo2.age)
+print(ceo2.address)
+
+print(ceo3.name)
+print(ceo3.surname)
+print(ceo3.age)
+print(ceo3.address)
+
+print(ceo4.name)
+print(ceo4.surname)
+print(ceo4.age)
+print(ceo4.address)
---
+++ b.py
@@ -0,0 +1,7 @@
+#1. class object is one time process
+class MyClass():
+    #1 Property=Variable
+    name="Vishal"
+    surname="Mahawar"
+    age=15
+    address="Neemuch"
